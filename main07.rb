704d012ad0116f137670a5e256213a741c69b481
Hossein <hosseinsalmani1373@gmail.com>
2021-06-04 11:53:24 +0430

    Amalgar

---
+++ main07.rb
@@ -0,0 +1,15 @@
+a=10
+b=2
+# puts a * b
+# puts a + b
+# puts a / b
+# puts a - b
+# puts a % b
+# puts a ** b
+# ===================
+# puts a == b
+# puts a != b
+# puts a > b
+# puts a < b
+# puts a <= b
+# puts a >= b
